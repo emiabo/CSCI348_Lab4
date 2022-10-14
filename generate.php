cd73c635e965ef5184e44c1c7fe6bdf6a6be51b0
Emi Aboudara <naboudara@gmail.com>
2022-10-14 11:54:18 -0700

    Checking input

--- generate.php
+++ generate.php
@@ -1,5 +1,15 @@
 <?php
 include 'header.php';
-
+include 'openers.php';
+include 'sentences.php';
+include 'closers.php';
+try {
+    if(strlen($_GET['fname']) == 0 || strlen($_GET['lname']) == 0) {
+        throw new Exception("Enter a full name!");
+    }
+    //Set the rest of the vars
+} catch(Exception $e) {
+    echo($e->getMessage());
+}
 include 'footer.php';
 ?>
