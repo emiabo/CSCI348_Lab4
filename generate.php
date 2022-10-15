a58ad617a23b54d78d2ada9ddf1a0559c8fb1322
Emilia Aboudara <naboudara@gmail.com>
2022-10-15 15:49:32 -0700

    Implemented generator

--- generate.php
+++ generate.php
@@ -3,11 +3,68 @@ include 'header.php';
 include 'openers.php';
 include 'sentences.php';
 include 'closers.php';
+function convertString($temp, $fname, $lname, $npro, $ppro, $opro) {
+    $temp2 = str_replace("_FNAME_", $fname, $temp);
+    $temp = str_replace("_LNAME_", $lname, $temp2);
+    $temp2 = str_replace("_NPRONOUN_", $npro,  $temp);
+    $temp = str_replace("_PPRONOUN_", $ppro, $temp2);
+    $temp2 = str_replace("_OPRONOUN_", $opro,  $temp);
+    
+    if (strpos($temp2, "<b>") == 0)
+    {
+      $temp = substr_replace($temp2, "", 0, 3);
+      $temp2 = ucfirst($temp); 
+      $temp = "<b>".$temp2; 
+    }
+    else
+    {
+      $temp = ucfirst($temp2); 
+    }
+
+    return $temp;
+}
 try {
     if(strlen($_GET['fname']) == 0 || strlen($_GET['lname']) == 0) {
         throw new Exception("Enter a full name!");
     }
     //Set the rest of the vars
+    $fname = ucfirst($_GET['fname']);
+    $lname = ucfirst($_GET['lname']);
+    $pronouns = explode(" ", $_GET['pronouns']);
+    list($npro, $opro, $ppro) = $pronouns;
+    if(strpos($opro, "fname") !== false) {
+        $opro = $fname . "'s";
+    }
+    $numSentences = $_GET['numSentences'];
+    //Get unformatted sentences
+    $opener = $openers[rand(0, 3)]['text'] . ".";
+    
+    $roasts = array();
+    $generated = 0;
+    $usedSentences = array();
+    while($generated < ($numSentences - 2)) {
+        $possible = rand(0, (count($sentences) - 1));
+
+        if (in_array($possible, $usedSentences) != true)
+        {
+          $usedSentences[count($usedSentences)] = $possible;
+        
+          $generated = $generated + 1;
+
+          $roasts[] = $sentences[$possible]["text"] . ".";
+        }
+    }
+    
+    $closer = $closers[rand(0, 3)]['text'] . ".";
+    //print the strings
+    echo(convertString($opener, $fname, $lname, $npro, $ppro, $opro) . "<br>");
+    echo("<ul>");
+    foreach($roasts as $roast) {
+        echo("<li>" . convertString($roast, $fname, $lname, $npro, $ppro, $opro) . "</li>");
+    }
+    echo("</ul>");
+    echo(convertString($closer, $fname, $lname, $npro, $ppro, $opro));
+    echo("<br><a href='lab4.php'>Try again</a>");
 } catch(Exception $e) {
     echo($e->getMessage());
 }
--- lab4.php
+++ lab4.php
@@ -10,20 +10,20 @@
     <br>
     <label for="pronouns">Pronouns:</label>
     <select name="pronouns" id="pronouns">
-        <option>He, him, his</option>
-        <option>He, they, firstname('s)</option>
-        <option>She, her, hers</option>
-        <option>She, they, firstname('s)</option>
-        <option>They, them, theirs</option>
-        <option>Ze, hir, hirs</option>
-        <option>Ze, zir, zirs</option>
+        <option value="he his him">He, him, his</option>
+        <option value="he fname they">He, they, firstname('s)</option>
+        <option value="she hers her">She, her, hers</option>
+        <option value="she fname they">She, they, firstname('s)</option>
+        <option value="they theirs them">They, them, theirs</option>
+        <option value="ze hirs hir">Ze, hir, hirs</option>
+        <option value="ze zirs zir">Ze, zir, zirs</option>
     </select>
     <br>
     <label for="numSentences">How many sentences:</label>
     <select name="numSentences" id="numSentences">
-        <option>3</option>
-        <option>5</option>
-        <option>10</option>
+        <option value="3">3</option>
+        <option value="5">5</option>
+        <option value="10">10</option>
     </select>
     <br>
     <input type="submit" value="Roast!">
